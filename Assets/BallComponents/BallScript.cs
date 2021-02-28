0b21df28aef0c8cf2563f39771b6df930949955a
CoderBee555 <ozhanacr@gmail.com>
2021-02-28 17:48:59 +0300

    Added Ball
    Added ball

---
+++ Assets/BallComponents/BallScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallScript : MonoBehaviour
+{
+
+    RaycastHit objectHit;
+
+   
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (Input.GetMouseButtonDown(0))
+        {
+
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out objectHit, Mathf.Infinity))
+            {
+                if(objectHit.collider.gameObject.tag == "Ball")
+                {
+                    Debug.Log("Topa Tıkladın laaan");
+                    Debug.Log(objectHit.transform.position);
+                }
+            }
+        }
+    }
+}
