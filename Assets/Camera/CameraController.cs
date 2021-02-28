2ee564cadb82818e73d39d02fcd60ff3667bca4a
umutyalcinn <74070401+umutyalcinn@users.noreply.github.com>
2021-02-28 20:04:52 +0300

    cameera init
    camera control script initialized

---
+++ Assets/Camera/CameraController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    public float sensivity;
+
+    private Vector3 firstTouchPos;
+    private Vector3 lastTouchPos;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.touchCount == 1)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            if(touch.phase == TouchPhase.Began)
+            {
+
+            }
+        }
+    }
+}
